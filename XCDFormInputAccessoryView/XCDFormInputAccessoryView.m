2b2e4e3f2cfee5dff808402ce0ba78905e09e0d1
Leonardo Formaggio <leoformaggio@gmail.com>
2014-05-16 18:20:23 -0300

    Fixing side-by-side and disabled fields

--- XCDFormInputAccessoryView/XCDFormInputAccessoryView.m
+++ XCDFormInputAccessoryView/XCDFormInputAccessoryView.m
@@ -7,20 +7,14 @@
 
 #import "XCDFormInputAccessoryView.h"
 
-static NSString * UIKitLocalizedString(NSString *string)
-{
-	NSBundle *UIKitBundle = [NSBundle bundleForClass:[UIApplication class]];
-	return UIKitBundle ? [UIKitBundle localizedStringForKey:string value:string table:nil] : string;
-}
-
 static NSArray * EditableTextInputsInView(UIView *view)
 {
 	NSMutableArray *textInputs = [NSMutableArray new];
 	for (UIView *subview in view.subviews)
 	{
-		BOOL isTextField = [subview isKindOfClass:[UITextField class]];
+		BOOL isEnabledTextField = [subview isKindOfClass:[UITextField class]] && [(UITextField *)subview isEnabled];
 		BOOL isEditableTextView = [subview isKindOfClass:[UITextView class]] && [(UITextView *)subview isEditable];
-		if (isTextField || isEditableTextView)
+		if (isEnabledTextField || isEditableTextView)
 			[textInputs addObject:subview];
 		else
 			[textInputs addObjectsFromArray:EditableTextInputsInView(subview)];
@@ -28,10 +22,15 @@ static NSArray * EditableTextInputsInView(UIView *view)
 	return textInputs;
 }
 
+@interface XCDFormInputAccessoryView ()
+
+@property (nonatomic, strong) UIToolbar *toolbar;
+@property (nonatomic, strong) UIBarButtonItem *previousBarButtonItem;
+@property (nonatomic, strong) UIBarButtonItem *nextBarButtonItem;
+
+@end
+
 @implementation XCDFormInputAccessoryView
-{
-	UIToolbar *_toolbar;
-}
 
 - (id) initWithFrame:(CGRect)frame
 {
@@ -45,23 +44,23 @@ static NSArray * EditableTextInputsInView(UIView *view)
 	
 	_responders = responders;
 	
-	_toolbar = [[UIToolbar alloc] init];
-	_toolbar.tintColor = nil;
-	_toolbar.barStyle = UIBarStyleBlack;
-	_toolbar.translucent = YES;
-	_toolbar.autoresizingMask = UIViewAutoresizingFlexibleWidth;
-	UISegmentedControl *segmentedControl = [[UISegmentedControl alloc] initWithItems:@[ UIKitLocalizedString(@"Previous"), UIKitLocalizedString(@"Next") ]];
-	[segmentedControl addTarget:self action:@selector(selectAdjacentResponder:) forControlEvents:UIControlEventValueChanged];
-	segmentedControl.segmentedControlStyle = UISegmentedControlStyleBar;
-	segmentedControl.momentary = YES;
-	UIBarButtonItem *segmentedControlBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:segmentedControl];
-	UIBarButtonItem *flexibleSpace = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
-	_toolbar.items = @[ segmentedControlBarButtonItem, flexibleSpace ];
+	self.toolbar = [[UIToolbar alloc] init];
+	self.toolbar.barStyle = UIBarStyleDefault;
+	self.toolbar.translucent = YES;
+	self.toolbar.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+	UIImage *previousImage = [UIImage imageNamed:@"XCDFormInputAccessoryView.bundle/button-arrow-left"];
+	UIImage *nextImage = [UIImage imageNamed:@"XCDFormInputAccessoryView.bundle/button-arrow-right"];
+	self.previousBarButtonItem = [[UIBarButtonItem alloc] initWithImage:previousImage style:UIBarButtonItemStylePlain target:self action:@selector(previous:)];
+	self.nextBarButtonItem = [[UIBarButtonItem alloc] initWithImage:nextImage style:UIBarButtonItemStylePlain target:self action:@selector(next:)];
+	UIBarButtonItem *space = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFixedSpace target:nil action:nil];
+	UIBarButtonItem *flexSpace = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+	space.width = 20;
+	self.toolbar.items = @[self.previousBarButtonItem, space, self.nextBarButtonItem, flexSpace];
 	self.hasDoneButton = [[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone;
 	
-	[self addSubview:_toolbar];
+	[self addSubview:self.toolbar];
 	
-	self.frame = _toolbar.frame = (CGRect){CGPointZero, [_toolbar sizeThatFits:CGSizeZero]};
+	self.frame = self.toolbar.frame = (CGRect){CGPointZero, [self.toolbar sizeThatFits:CGSizeZero]};
 	
 	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textInputDidBeginEditing:) name:UITextFieldTextDidBeginEditingNotification object:nil];
 	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textInputDidBeginEditing:) name:UITextViewTextDidBeginEditingNotification object:nil];
@@ -80,11 +79,8 @@ static NSArray * EditableTextInputsInView(UIView *view)
 	if ([responders count] == 0)
 		return;
 	
-	UISegmentedControl *segmentedControl = (UISegmentedControl *)[_toolbar.items[0] customView];
-	BOOL isFirst = [[responders objectAtIndex:0] isFirstResponder];
-	BOOL isLast = [[responders lastObject] isFirstResponder];
-	[segmentedControl setEnabled:!isFirst forSegmentAtIndex:0];
-	[segmentedControl setEnabled:!isLast forSegmentAtIndex:1];
+	self.previousBarButtonItem.enabled = ![responders.firstObject isFirstResponder];
+	self.nextBarButtonItem.enabled = ![responders.lastObject isFirstResponder];
 }
 
 - (void) willMoveToWindow:(UIWindow *)window
@@ -113,16 +109,22 @@ static NSArray * EditableTextInputsInView(UIView *view)
 		
 		CGRect frame1 = [textInput1 convertRect:textInput1.bounds toView:commonAncestorView];
 		CGRect frame2 = [textInput2 convertRect:textInput2.bounds toView:commonAncestorView];
-		return [@(CGRectGetMinY(frame1)) compare:@(CGRectGetMinY(frame2))];
+		
+		NSComparisonResult result = [@(CGRectGetMinY(frame1)) compare:@(CGRectGetMinY(frame2))];
+		if (result == NSOrderedSame) {
+			result = [@(CGRectGetMinX(frame1)) compare:@(CGRectGetMinX(frame2))];
+		}
+		
+		return result;
 	}];
 }
 
-- (void) setHasDoneButton:(BOOL)hasDoneButton
+- (void)setHasDoneButton:(BOOL)hasDoneButton
 {
 	[self setHasDoneButton:hasDoneButton animated:NO];
 }
 
-- (void) setHasDoneButton:(BOOL)hasDoneButton animated:(BOOL)animated
+- (void)setHasDoneButton:(BOOL)hasDoneButton animated:(BOOL)animated
 {
 	if (_hasDoneButton == hasDoneButton)
 		return;
@@ -132,25 +134,41 @@ static NSArray * EditableTextInputsInView(UIView *view)
 	[self didChangeValueForKey:@"hasDoneButton"];
 	
 	NSArray *items;
-	if (hasDoneButton)
-		items = [_toolbar.items arrayByAddingObject:[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(done)]];
-	else
-		items = [_toolbar.items subarrayWithRange:NSMakeRange(0, 2)];
+	if (hasDoneButton) {
+		UIBarButtonItem *done = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(done)];
+		items = [self.toolbar.items arrayByAddingObject:done];
+	} else {
+		items = [self.toolbar.items subarrayWithRange:NSMakeRange(0, 2)];
+	}
 	
-	[_toolbar setItems:items animated:animated];
+	[self.toolbar setItems:items animated:animated];
 }
 
 #pragma mark - Actions
 
-- (void) selectAdjacentResponder:(UISegmentedControl *)sender
+- (void)previous:(id)sender
 {
 	NSArray *firstResponders = [self.responders filteredArrayUsingPredicate:[NSPredicate predicateWithBlock:^BOOL(UIResponder *responder, NSDictionary *bindings) {
 		return [responder isFirstResponder];
 	}]];
 	UIResponder *firstResponder = [firstResponders lastObject];
-	NSInteger offset = sender.selectedSegmentIndex == 0 ? -1 : +1;
 	NSInteger firstResponderIndex = [self.responders indexOfObject:firstResponder];
-	NSInteger adjacentResponderIndex = firstResponderIndex != NSNotFound ? firstResponderIndex + offset : NSNotFound;
+	NSInteger adjacentResponderIndex = firstResponderIndex != NSNotFound ? firstResponderIndex - 1 : NSNotFound;
+	UIResponder *adjacentResponder = nil;
+	if (adjacentResponderIndex >= 0 && adjacentResponderIndex < (NSInteger)[self.responders count])
+		adjacentResponder = [self.responders objectAtIndex:adjacentResponderIndex];
+	
+	[adjacentResponder becomeFirstResponder];
+}
+
+- (void)next:(id)sender
+{
+	NSArray *firstResponders = [self.responders filteredArrayUsingPredicate:[NSPredicate predicateWithBlock:^BOOL(UIResponder *responder, NSDictionary *bindings) {
+		return [responder isFirstResponder];
+	}]];
+	UIResponder *firstResponder = [firstResponders lastObject];
+	NSInteger firstResponderIndex = [self.responders indexOfObject:firstResponder];
+	NSInteger adjacentResponderIndex = firstResponderIndex != NSNotFound ? firstResponderIndex + 1 : NSNotFound;
 	UIResponder *adjacentResponder = nil;
 	if (adjacentResponderIndex >= 0 && adjacentResponderIndex < (NSInteger)[self.responders count])
 		adjacentResponder = [self.responders objectAtIndex:adjacentResponderIndex];
